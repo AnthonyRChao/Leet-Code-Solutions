0e26a9c29df2aedab5e6eedc34972b650c2512e9
ARC <anthony.r.chao@gmail.com>
2018-11-02 16:57:08 -0700

    Update and rename sort_array_by_parity to sort_array_by_parity.py

---
+++ sort_array_by_parity.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/sort-array-by-parity/
+
+
+class Solution:
+    def sortArrayByParity(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        even = []
+        odd = []
+        
+        for value in A:
+            if value % 2 == 0:
+                even.append(value)
+            else:
+                odd.append(value)
+        
+        return even + odd
+
+"""
+Provided Solution
+
+class Solution(object):
+    def sortArrayByParity(self, A):
+        A.sort(key = lambda x: x % 2)
+        return A
+
+_Learnings: Use a custom comparator when sorting, to sort by parity.
+"""
+        
+ 
